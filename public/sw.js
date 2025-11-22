e98d36735596ed99bd82017e18bb1ed79bd56602
TheDopeyOne <omercevahir84@gmail.com>
2025-11-22 12:39:56 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,12 @@
+// Kurulum Olayı: Tarayıcı bu dosyayı görünce çalıştırır
 self.addEventListener('install', (event) => {
   console.log('Hizmetli (Service Worker) yüklendi!');
+  // Bekleme yapma, hemen devreye gir komutu
   self.skipWaiting();
 });
 
+// Yakalama Olayı: İnternet trafiğini izler
 self.addEventListener('fetch', (event) => {
-  // Burası boş kalsa bile Chrome mutlu olur :)
+  // Burayı boş bıraktık. Normalde çevrimdışı (offline) çalışma kodları buraya yazılır.
+  // Chrome, içi boş olsa bile bu "fetch" olayını görünce "Tamam, bu bir PWA" der.
 });
